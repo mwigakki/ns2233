3bd3c2214d70728ad275610140f46a434624aa3e
root <root@localhost.localdomain>
2022-09-20 09:42:55 +0800

    update websit 9.20

--- resources/index.js
+++ resources/index.js
@@ -1,2 +1,16 @@
-var card = document.getElementsByClassName("function-card");
-
+function readTextFile(fileName) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", fileName, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
+        words = allText.split("\n"); // 0:title, 1:author, 2:paragraph1
+        $(".read-title").text(words[0]);
+        $(".read-content").text(words[2]);
+      }
+    }
+  };
+  rawFile.send(null);
+}
+window.onload = readTextFile("resources/dailyread.txt"); // 读取本地txt文件
